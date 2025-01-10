6770025a4b17a365ff8deee334504b78e312400e
unknown <mharfemicaroz@gmail.com>
2025-01-10 17:25:58 +0800

    update blacklisted

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
 const express = require("express");
 const app = express();
 
-// Example list of blacklisted IPs
-const blacklistedIPs = [
-  "192.168.1.1", // Example blacklisted IP
-  "120.28.196.158", // Example blacklisted IP
-  // Add more blacklisted IPs here
-];
-
-// Middleware to log the IP address of the request and check against blacklist
+// Middleware to log the IP address of the request
 app.use((req, res, next) => {
   // Vercel uses a proxy, so we need to check the X-Forwarded-For header for the real client IP
   const ipAddress =
     req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-
   console.log(`Request received from IP address: ${ipAddress}`);
-
-  // Check if the IP address is blacklisted
-  if (blacklistedIPs.includes(ipAddress)) {
-    // If the IP is blacklisted, deny access
-    return res.json({ access: 0 });
-  }
-
-  // If the IP is not blacklisted, allow access
   next();
 });
 
