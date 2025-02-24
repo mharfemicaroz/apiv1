1ea74d0fd561a7e0707ee9d26319c20c2789deef
unknown <mharfemicaroz@gmail.com>
2025-02-24 16:40:52 +0800

    update ip add

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const whitelistedIPRanges = [
   "49.146.5.",
   "61.245.13.",
   "222.127.28.",
+  "143.44.184.",
   "49.146.", // Added broader IP pattern
 ]; // Add ranges (e.g., 216.247.50.xxx)
 
